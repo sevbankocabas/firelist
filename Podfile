627a27e56fbb6cb11206f7fe9eaf17beb75b8c80
Sevban Kocabaş <sevban@kukapps.com>
2018-12-30 14:19:04 +0300

    initial commit

---
+++ Podfile
@@ -0,0 +1,15 @@
+project 'firelist.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'firelist' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for firelist
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Database'
+
+end
---
+++ firelist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firelist
+//
+//  Created by Sevban Kocabaş on 30.12.2018.
+//  Copyright © 2018 Sevban Kocabaş. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
